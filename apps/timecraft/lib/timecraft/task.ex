01840d551b147ec2152f4e998054fab6b9b1b82d
Kelvin Stinghen <kelvin.stinghen@me.com>
2020-11-28 08:12:52 -0300

    Reading task projects (#5)

--- apps/timecraft/lib/timecraft/task.ex
+++ apps/timecraft/lib/timecraft/task.ex
@@ -1,3 +1,3 @@
 defmodule Timecraft.Task do
-  defstruct ~w(external_id completed title notes due_date due_time order)a
+  defstruct ~w(external_id completed title notes due_date due_time order project)a
 end
