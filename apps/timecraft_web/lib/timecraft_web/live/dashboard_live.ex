e60562bfcb5615db037ccc2e1786658c90a18647
kelvinst <kelvin.stinghen@gmail.com>
2020-12-06 15:54:33 -0300

    Better UX for tasks

--- apps/timecraft_web/lib/timecraft_web/live/dashboard_live.ex
+++ apps/timecraft_web/lib/timecraft_web/live/dashboard_live.ex
@@ -6,7 +6,7 @@ defmodule TimecraftWeb.DashboardLive do
   @impl true
   def mount(_params, _session, socket) do
     date = DateUtils.today()
-    {:ok, assign(socket, tasks: Timecraft.tasks(date), date: date)}
+    {:ok, assign(socket, tasks: nil, date: date)}
   end
 
   @impl true
@@ -16,25 +16,36 @@ defmodule TimecraftWeb.DashboardLive do
 
   defp apply_action(socket, :show, %{"date" => str}) do
     date = Date.from_iso8601!(str)
+    async_load_tasks(date)
 
     socket
-    |> assign(:page_title, "Tasks #{date}")
+    |> assign(:page_title, to_string(date))
     |> assign(:date, date)
-    |> assign(:tasks, Timecraft.tasks(date))
+    |> assign(:tasks, nil)
   end
 
   defp apply_action(socket, :index, _params) do
     date = DateUtils.today()
+    async_load_tasks(date)
 
     socket
-    |> assign(:page_title, "Tasks #{date}")
+    |> assign(:page_title, to_string(date))
     |> assign(:date, date)
-    |> assign(:tasks, Timecraft.tasks(date))
+    |> assign(:tasks, nil)
+  end
+
+  defp async_load_tasks(date) do 
+    send(self(), {:load_tasks, date})
   end
 
   @impl true
   def handle_event("resolve_time_entries_conflicts", _, socket) do
     Timecraft.resolve_time_entries_conflicts(socket.assigns.date)
     {:reply, %{}, put_flash(socket, :success, "Time entries conflicts successfully resolved!")}
+  end
+
+  @impl true
+  def handle_info({:load_tasks, date}, socket) do
+    {:noreply, assign(socket, :tasks, Timecraft.tasks(date))}
   end
 end
